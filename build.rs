b6cf79956282eaf0000ff90b0712f0bac040bd79
fsnkty <fsnkty@shimeji.cafe>
2024-11-20 19:10:49 +1300

    embed windows exe icon

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ extern crate embed_resource;
 fn main() {
     let target = std::env::var("TARGET").unwrap();
     if target.contains("windows") {
-        embed_resource::compile("build-assets/icon.rc");
+        embed_resource::compile("build-assets/icon.rc", embed_resource::NONE).manifest_optional().unwrap();
     }
 }
